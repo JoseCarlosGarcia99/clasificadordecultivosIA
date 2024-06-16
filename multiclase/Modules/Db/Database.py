76a43835953b8cc2877ba9c324f387cf3db08f37
JoseCarlos <josecarlosgarciavelez.99@gmail.com>
2024-06-15 22:44:09 -0600

    Solucion del problema de seguridad de AWS

--- multiclase/Modules/Db/Database.py
+++ multiclase/Modules/Db/Database.py
@@ -2,6 +2,6 @@ from boto3 import resource
 from os import getenv
 
 dynamodb = resource("dynamodb", 
-                     aws_access_key_id="AKIA5ALIJLLGCKX4F6IN",
-                     aws_secret_access_key="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", 
-                     region_name="us-east-2")
+                     aws_access_key_id="ID_KEY",
+                     aws_secret_access_key="ACCES_KEY", 
+                     region_name="REGION")
